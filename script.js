eab3dc7937efd52059f051c557aa6a6cab662019
Keago99 <keatabde@gmail.com>
2024-12-10 10:12:21 +0200

    add basic tests

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+function captialize(string){
+    return toUpperCase(string);
+}
+
+function reverseString(){
+
+}
+
+const calculator = {
+    add: (a,b) =>{
+        return (a+b);
+    },
+
+    subtract: (a,b) =>{
+        return (a-b);
+    },
+
+    divide: (a,b) => {
+        return (a/b);
+    },
+    
+    multiply: (a,b) => {
+        return (a*b);
+    },
+}
+
+module.exports = capitalize;
