0ea9a996039def7bbe9fe3433841d137319cd955
Keago99 <keatabde@gmail.com>
2024-12-13 22:47:49 +0200

    non working ceasarCipher solution, time to look at other code to learn.

--- script.js
+++ script.js
@@ -18,7 +18,12 @@ let calculator = {
     },
 
     divide(a,b){
-        return (a/b);
+        if (b === 0){
+            return ("Cannot divide by zero")
+        }
+        else{
+            return (a/b);
+        }
     },
     
     multiply(a,b){
@@ -26,4 +31,37 @@ let calculator = {
     },
 }
 
-module.exports = {captialize, reverseString, calculator};
+function caesarCipher(word,shift){
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    splitArray = word.split();
+    let newWord = [];
+    let isUpper = false;
+
+
+
+    for(let i = 0; i < splitArray.length; i++){
+        let checkword = splitArray[i];
+        if (checkword === checkword.toUpperCase()){
+            isUpper = true;
+            checkword = checkword.toLowerCase();
+        }
+        if ((alphabet.indexOf(checkword)) != -1){
+            let oldindex = alphabet.indexOf(checkword);
+            let newindex = oldindex + shift
+            while(newindex > 26){
+                newindex = newindex - 26;
+            }
+            if(isUpper === true){
+                newLetter = (alphabet[newindex].toUpperCase());
+                newWord.push(newLetter);
+                isUpper = false;
+            }else{
+                newLetter = (alphabet[newindex]);
+                newWord.push(newLetter);
+            }
+        }
+    }
+    return newWord;
+}
+
+module.exports = {captialize, reverseString, calculator, caesarCipher};
--- script.test.js
+++ script.test.js
@@ -11,3 +11,15 @@ test("String reversal", () => {
 test("Calculator addition", () => {
     expect(functions.calculator.add(1,2)).toBe(3);
 });
+
+test("Calculator subtraction", () => {
+    expect(functions.calculator.subtract(5,3)).toBe(2);
+});
+
+test("Calculator multiplication", () => {
+    expect(functions.calculator.multiply(5,3)).toBe(15);
+});
+
+test("CaesarCipher very basic shift", () => {
+    expect(functions.caesarCipher("AB",1)).toBe("BC");
+});
