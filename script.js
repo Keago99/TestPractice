25435720e8fd9f9a4f24265ce5d8b229d1643ad7
Keago99 <keatabde@gmail.com>
2024-12-14 14:13:10 +0200

    Remove ceasarCipher method from scripts.js create separate javascript file for ceasarCipher Test each line indavidually while reading documentation to ensure proper implementation.

--- script.js
+++ script.js
@@ -31,37 +31,4 @@ let calculator = {
     },
 }
 
-function caesarCipher(word,shift){
-    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    splitArray = word.split();
-    let newWord = [];
-    let isUpper = false;
-
-
-
-    for(let i = 0; i < splitArray.length; i++){
-        let checkword = splitArray[i];
-        if (checkword === checkword.toUpperCase()){
-            isUpper = true;
-            checkword = checkword.toLowerCase();
-        }
-        if ((alphabet.indexOf(checkword)) != -1){
-            let oldindex = alphabet.indexOf(checkword);
-            let newindex = oldindex + shift
-            while(newindex > 26){
-                newindex = newindex - 26;
-            }
-            if(isUpper === true){
-                newLetter = (alphabet[newindex].toUpperCase());
-                newWord.push(newLetter);
-                isUpper = false;
-            }else{
-                newLetter = (alphabet[newindex]);
-                newWord.push(newLetter);
-            }
-        }
-    }
-    return newWord;
-}
-
-module.exports = {captialize, reverseString, calculator, caesarCipher};
+module.exports = {captialize, reverseString, calculator};
